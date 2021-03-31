28e9347206dd98e082e17bd600919a686f9baeeb
Mordashkoav <mordashkoav@gmail.com>
2021-03-31 14:14:46 +0300

    первое дз по js

---
+++ index.js
@@ -0,0 +1,38 @@
+let str='Привет'
+console.log(typeof str)
+let num=123
+console.log(typeof num)
+let flag=true
+console.log(typeof flag)
+let txt='true'
+console.log(typeof txt)
+let a1=5+3
+console.log(a1)
+let a2=5-3
+console.log(a2)
+let a3=5*3
+console.log(a3)
+let a4=Math.round(5/3)
+console.log(a4)
+let a5=5%3
+console.log(a5)
+let a6=3%5
+console.log(a6)
+let a7=5+'3'
+console.log(a7)
+let a8='5'+3
+console.log(a8)
+let a9=75+'кг'
+console.log(a9)
+let height=23
+let width=10
+let s=`s=${height*width}см`
+console.log(s)
+let heightC=4
+let dC=10
+let v=`v=${heightC*dC}м`
+console.log(v)
+let n=3
+let m=4
+let k=Math.sqrt(n*n+m*m)
+console.log(`k=${k}`) 
